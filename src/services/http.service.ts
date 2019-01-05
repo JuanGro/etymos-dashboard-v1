f9f8376a73fde455267fd035bafe02eefb30e1ec
juangro <ing.juangro@gmail.com>
2019-01-05 17:57:07 -0600

    Fix error handling

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -1,24 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 import { environment } from './../environments/environment';
-import { ErrorService } from './error.service';
+import { ToasterService } from './toaster.service';
 
 @Injectable()
-export class HttpService extends ErrorService {
+export class HttpService {
 
-  constructor(private http: HttpClient) {
-    super();
-  }
+  constructor(private http: HttpClient, private toasterService: ToasterService) {}
 
   get(urlGET: string) {
     return this.http.get(environment.API_URL + urlGET)
-      .pipe(catchError(this.handleError));
+      .pipe(
+        catchError(err => {
+          this.toasterService.showError("Error", "Hubo un error en la petición GET");
+          return throwError(err);
+          }))
   }
 
   post(urlPOST: string, postObject: any) {
     return this.http.post(environment.API_URL + urlPOST, postObject)
-      .pipe(catchError(this.handleError));
+      .pipe(
+        catchError(err => {
+          this.toasterService.showError("Error", "Hubo un error en la petición POST");
+          return throwError(err);
+        }));
   }
 }
