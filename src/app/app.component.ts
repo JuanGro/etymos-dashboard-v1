036399d3bfc601f1e55253d62504e9c477ee61c1
juangro <ing.juangro@gmail.com>
2019-01-04 19:36:43 -0600

    Add wizard correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Etymos-API-Requests';
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
 }
