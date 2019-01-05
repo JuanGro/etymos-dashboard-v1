171d72b62b0e9cb84f134517c918622d9a339b2b
juangro <ing.juangro@gmail.com>
2019-01-04 21:42:29 -0600

    Add test form with options and modify messages and model

--- src/models/test.ts
+++ src/models/test.ts
@@ -2,12 +2,18 @@ export class Test {
   public phrase: string;
   public word_id: number;
   public etymos_version_test: string;
+  public option1: string;
+  public option2: string;
+  public option3: string;
 
   constructor(json?: Test) {
     if (json) {
       this.phrase = json.phrase;
       this.word_id = json.word_id;
       this.etymos_version_test = json.etymos_version_test;
+      this.option1 = json.option1;
+      this.option2 = json.option2;
+      this.option3 = json.option3;
     }
   }
 }
